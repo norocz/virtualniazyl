e336b1ef1a50d0c814d0f14855d010d8f7553384
Noro_CZ <46658812+JosefNemc@users.noreply.github.com>
2024-10-07 23:03:13 +0200

    Delete vaz/vendor/nepada/phone-number-input/src/assets directory

--- vaz/vendor/nepada/phone-number-input/src/assets/index.js
+++
@@ -1,7 +0,0 @@
-import bindToNetteForms from './bindToNetteForms';
-import parsePhoneNumberFactory from './parsePhoneNumberFactory';
-import {parsePhoneNumberFromString} from 'libphonenumber-js/core';
-
-export default (Nette, metadata) => {
-    bindToNetteForms(Nette, parsePhoneNumberFactory(parsePhoneNumberFromString, metadata));
-};
