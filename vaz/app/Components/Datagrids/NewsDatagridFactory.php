760939c84439abacf232b18d88c9333be323a77e
Noro <foto@josefnemec.cz>
2025-04-01 21:30:44 +0200

    chyba v News Datagridu (připnutá Pined)

--- vaz/app/Components/Datagrids/NewsDatagridFactory.php
+++ vaz/app/Components/Datagrids/NewsDatagridFactory.php
@@ -132,7 +132,7 @@ class NewsDatagridFactory extends DataGrid
                 ->endOption()
             ->onChange[] = [$this, 'importantNewsChange'];
 
-        $this->addColumnStatus('pined','pined', 'Připnutá')
+        $this->addColumnStatus('pined','Připnutá', 'pined')
             ->setTemplate(__DIR__ .'/templates/column_status.latte')
             ->addOption(true, 'Ano')
             ->setClass('btn-sm btn-warning')
